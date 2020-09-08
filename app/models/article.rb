628ac3cdffb17792fed98534b09bc1e95aa0bc4a
今野郷 <xiangjinye1@gmail.com>
2020-09-09 07:00:57 +0900

    フォロー機能、フォロー一覧、フォロワー一覧

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,6 +12,10 @@ class Article < ApplicationRecord
 
     belongs_to :user
 
+    def author_name
+        user.display_name
+    end
+
     def display_created_at
         I18n.l(self.created_at, format: :default)
     end
