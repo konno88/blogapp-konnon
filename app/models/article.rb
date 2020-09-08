0e1456b6a1c1f8846312747469304bd8895ddfdb
今野郷 <xiangjinye1@gmail.com>
2020-09-09 04:48:53 +0900

    記事の一覧、投稿、編集、削除

---
+++ app/models/article.rb
@@ -0,0 +1,18 @@
+class Article < ApplicationRecord
+
+    validates :title, presence: true
+    validates :title, length: {minimum: 2, maximum: 100}
+    validates :title, format: {with: /\A(?!\@)/}
+    validates :content, presence: true
+
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
+    has_one_attached :eyecatch
+    has_rich_text :content
+
+    belongs_to :user
+
+    def display_created_at
+        I18n.l(self.created_at, format: :default)
+    end
+end
