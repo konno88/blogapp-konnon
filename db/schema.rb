14ebba41d85b33173a888255e44646cc9f489fe0
今野郷 <xiangjinye1@gmail.com>
2020-09-09 06:32:28 +0900

    プロフィール作成、編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_193420) do
+ActiveRecord::Schema.define(version: 2020_09_08_212127) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,20 @@ ActiveRecord::Schema.define(version: 2020_09_08_193420) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_likes_on_article_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "nickname"
+    t.text "introduction"
+    t.integer "gender"
+    t.integer "location"
+    t.integer "musical_instrument"
+    t.date "birthday"
+    t.boolean "subscribed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
